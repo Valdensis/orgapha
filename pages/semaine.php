c178d208ff2f8c8cc7178b71dc9e076ccfae0c7d
David Mack <david.kcam@gmail.com>
2016-06-13 22:59:18 +0200

    création de semaine.php, vide.
    Faut avancer back.brique.php

---
+++ pages/semaine.php
@@ -0,0 +1,15 @@
+<?php
+/************************************************************\
+ *
+ * File				semaine.php
+ *
+ * Language			PHP
+ *
+ * Author			David Mack
+ * Creation			12 juin 2016
+ * modification 
+ *
+ * Project			orgapha
+ *
+ \************************************************************/
+echo 'semaine.php';
